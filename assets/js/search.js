327fde1e9f379902ea810eff1845408ea83de3f3
der0pa <der0pa@users.noreply.github.com>
2020-05-05 18:44:01 +0000

    deploy: 77728f4a2d4330e8a4a5d5faebd738c55c419831

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/ML/assets/js/search-data.json', true);
+    request.open('GET', '/Machine-Learning/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
